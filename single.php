114e504f429adb4bcdff6a344fa75010cfe2515c
unknown <cdpsk8@gmail.com>
2020-01-15 14:40:52 -0500

    ajuste proyectos

--- single.php
+++ single.php
@@ -24,14 +24,21 @@ $estado = get_field('estado');
 $ciudad = get_field('ciudad');
 $relacionados = get_field('proyectos_relacionados');
 $coords = get_field('proj_latitude');
+$coords2 = get_field('proj_latitude2');
 $address = get_field('direccion_del_proyecto');
+$address2 = get_field('direccion_2_del_proyecto');
+$redes = get_field('redes');
+$zonas_comunes = get_field('descripcion_zonas_comunes');
+$servicios = get_field('servicios');
+$ubicacion2 = get_field('ubicacion2');
+$beneficios = get_field('descripcion_beneficios');
 ?>
 <div class="w-1000 c_detalle">
   <div class="miga antialias">
     <a href="javascript:void(0);">Home</a> / <a href="<?php echo $home_url; ?>/proyectos"><span>Proyectos</span></a> / <a href="<?php echo $$link; ?>"><span><?php echo $nombre; ?></span></a>
   </div>
   <div class="c_slide_principal">
-    <img src="<?php echo get_field('logo_proyecto'); ?>" class="logo_landing">
+
     <div id="owl-detalle" class="owl-carousel owl-theme">
       <div class="item"><img src="<?php echo get_field('imagen_principal'); ?>" class="bg"></div>
     <?php if(count($fotos)>0){
@@ -40,10 +47,53 @@ $address = get_field('direccion_del_proyecto');
     <?php }} ?>
     </div>
   </div>
+
+  <div class="c1_detalle clearfix">
+
+    <div class="left cont_2">
+      <img src="<?php echo get_field('imagen_descripcion'); ?>">
+    </div>
+    <div class="left cont_1">
+      <h3 class="antialias tit"><?php echo get_field('slogan'); ?></h3>
+      <?php echo get_field('descripcion');?>
+    </div>
+  </div>
+</div>
+<?php if(!empty($areas)>0){?>
+<div class="w-1000">
+  <div class="c_medidas center">
+    <div id="owl-medidas" class="owl-carousel">
+      <?php foreach($areas as $a){ ?>
+        <div class="item"><span><?php echo $a['descripcion_boton']; ?></span></div>
+      <?php } ?>
+    </div>
+  </div>
+  <div class="planos">
+    <div id="owl-planos" class="owl-carousel">
+    <?php foreach($areas as $a){ ?>
+        <div class="item clearfix">
+          <p><?php echo $a['descripcion_render']; ?></p>
+          <a href="<?php echo $a['imagen']; ?>" class="zoom_img swipebox" title="<?php echo $a['nombre']; ?>">
+            <img src="<?php echo $a['imagen']; ?>" alt=""> <span><img src="<?php bloginfo('template_url'); ?>/images/ico_zoom.png" alt=""></span>
+          </a>
+
+        </div>
+
+        <?php } ?>
+    </div>
+  </div>
+</div>
+  <?php } ?>
+<div class="w-1000">
   <div class="formulario c1_detalle clearfix">
-    <div class="item-formulario form-left">
-      <h3 class="antialias tit">CONTÁCTENOS</h3>
-      <p>Déjenos sus datos y lo contactaremos para darle mayor información acerca de nuestro proyecto.</p>
+    <div class="item-formulario form-left" style="padding: 5%; ">
+
+      <p style="font-size: 26px; text-align:center;">¡Queremos saber más sobre tus sueños
+de vivienda!</p>
+<p style="font-size: 26px; margin-top:20px;">Déjanos tus datos y con mucho gusto
+nos contactaremos contigo y te
+contaremos sobre nuestros proyectos.
+</p>
     </div>
     <div class="item-formulario form-right">
       <div class="container-formulario">
@@ -60,17 +110,7 @@ $address = get_field('direccion_del_proyecto');
     </div>
 
   </div>
-  <div class="c1_detalle clearfix">
-    <div class="left cont_1">
-      <h3 class="antialias tit"><?php echo get_field('slogan'); ?></h3>
-      <?php echo get_field('descripcion');?>
-    </div>
-    <div class="left cont_2">
-      <img src="<?php echo get_field('imagen_descripcion'); ?>">
-    </div>
-  </div>
 </div>
-
 
 <div class="ventajas">
   <div class="w-1000">
@@ -81,58 +121,72 @@ $address = get_field('direccion_del_proyecto');
         <h4>UBICACIÓN</h4>
         <?php echo get_field('descripcion_ubicacion'); ?>
       </li>
+      <?php if($ubicacion2): ?>
+      <li>
+        <img src="<?php bloginfo('template_url'); ?>/images/i1.png" alt="">
+        <h4>UBICACIÓN 2</h4>
+      <?php echo $ubicacion2 ?>
+      </li>
+    <?php endif; ?>
+      <?php if($zonas_comunes): ?>
       <li>
         <img src="<?php bloginfo('template_url'); ?>/images/i2.png" alt="">
         <h4>ZONAS COMUNES</h4>
-        <?php echo get_field('descripcion_zonas_comunes'); ?>
+        <?php echo $zonas_comunes ?>
       </li>
+    <?php endif; ?>
+    <?php if($servicios): ?>
+    <li>
+      <img src="<?php bloginfo('template_url'); ?>/images/s1.png" alt="">
+      <h4>SERVICIOS</h4>
+
+      <?php echo $servicios ?>
+    </li>
+  <?php endif; ?>
+  <?php if($beneficios): ?>
       <li>
         <img src="<?php bloginfo('template_url'); ?>/images/i3.png" alt="">
         <h4>BENEFICIOS</h4>
-        <?php echo get_field('descripcion_beneficios'); ?>
+        <?php echo $beneficios; ?>
       </li>
+    <?php endif; ?>
     </ul>
   </div>
 </div>
-<?php if(!empty($areas)>0){?>
-<div class="w-1000">
-  <div class="c_medidas center">
-    <div id="owl-medidas" class="owl-carousel">
-      <?php foreach($areas as $a){ ?>
-        <div class="item"><span><?php echo $a['descripcion_boton']; ?></span></div>
-      <?php } ?>
-    </div>
-  </div>
-  <div class="planos">
-    <div id="owl-planos" class="owl-carousel">
-    <?php foreach($areas as $a){ ?>
-        <div class="item clearfix">
-          <a href="<?php echo $a['imagen']; ?>" class="zoom_img swipebox" title="<?php echo $a['nombre']; ?>">
-            <img src="<?php echo $a['imagen']; ?>" alt=""> <span><img src="<?php bloginfo('template_url'); ?>/images/ico_zoom.png" alt=""></span>
-          </a>
-        </div>
-        <?php } ?>
-    </div>
-  </div>
-</div>
-<?php }
 
-// Build map 
+
+<?php
+
+// Build map
 if($coords){
   ?>
-  
+
   <div class="map_wrapper container">
     <div id="theMap" class="map_wrapper__embed" data-coords="<?php echo $coords; ?>" data-dir="<?php echo $address; ?>"></div>
   </div>
 
-  <?php  
+  <?php
 } ?>
+<?php
 
-<ul class="reset_list links_detalle center">
-  <li><a href="javascript:void(0);" class="btn" onclick="Contacto();">CONTÁCTENOS</a></li>
-  <!--<li><a href="javascript:void(0);" class="btn" onclick="miMail();">ENVIAR A MI EMAIL</a></li>-->
-  <li><a href="javascript:void(0);" class="btn" onclick="compartir();">COMPARTIR EN REDES</a></li>
-</ul>
+// Build map2
+if($coords2){
+  ?>
+
+  <div class="map_wrapper container">
+    <h2>UBICAICÓN 2</h2>
+    <div id="theMap2" class="map_wrapper__embed" data-coords="<?php echo $coords2; ?>" data-dir="<?php echo $address2; ?>"></div>
+  </div>
+
+  <?php
+} ?>
+<div class="social">
+  <div>
+    <?php foreach($redes as $r){ ?>
+      <div class="social-item"><a href="<?php echo $r['link']; ?>" target="_blank" class="<?php echo $r['nombre_red']; ?>" aria-hidden="true"></a></div>
+    <?php } ?>
+ </div>
+</div>
 <?php
 if(!empty($relacionados)>0){ ?>
 <div class="w-1000 relacionados">
@@ -180,6 +234,7 @@ $barrio = get_field('zona_o_barrio',$id_proyecto[0]);
     </div>
   </div>
 </div>
+
 <?php } //Landing ?>
 
 </section>
